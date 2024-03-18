be30f1d775042ae7938d95b2eb0948bdfac0f965
Tyler Tracy <tylertracy1999@gmail.com>
2024-03-18 00:05:10 -0500

    Move get fake data

--- transformer.py
+++ transformer.py
@@ -75,12 +75,6 @@ class Transformer(nn.Module):
         res = self.mlp(res) + self.layer_norm(res)
         return self.lm_head(res)
         
-def get_fake_data(args):
-    # list of tuples of (batch, labels)
-    batches = [torch.randn((args.bs, args.seq_len, args.hidden_dim)) for _ in range(10)]
-    labels = [torch.randint(0, 50257, (args.bs, args.seq_len)) for _ in range(10)]
-    return list(zip(batches, labels))
-
 def train_epoch(model, train_loader, optimizer):
     model.train()
     loss_sum = 0
