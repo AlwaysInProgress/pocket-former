97432666e2ec5a2b82485b9b2ad2453158cdb88e
Jeremy Collins <jacollins1999@gmail.com>
2024-03-21 23:46:54 -0400

    live model runs, add frames per item arg

--- turn_classifier_trainer.py
+++ turn_classifier_trainer.py
@@ -68,6 +68,7 @@ if __name__ == "__main__":
     parser.add_argument("--train", action="store_true")
     parser.add_argument("--checkpoint", type=str, default=None)
     parser.add_argument("--live", action="store_true")
+    parser.add_argument("--frames_per_item", type=int, default=10)
     args = parser.parse_args()
 
 
@@ -77,33 +78,41 @@ if __name__ == "__main__":
     print(model)
 
     if args.train:
-        pipeline = DataPipeline(frames_per_item=2)
+        pipeline = DataPipeline(frames_per_item=args.frames_per_item)
         train_dataset = pipeline.get_dataset('train')
         val_dataset = pipeline.get_dataset('test')
         print("train dataset length: ", len(train_dataset))
         print("val dataset length: ", len(val_dataset))
         train_loader = DataLoader(train_dataset, batch_size=args.bs, shuffle=True)
-        val_loader = DataLoader(train_dataset, batch_size=args.bs, shuffle=True)
+        val_loader = DataLoader(val_dataset, batch_size=args.bs, shuffle=True)
         train(model, args, train_loader, val_loader, device)
     elif args.live:
         model.eval()
         # live inference
         with torch.no_grad():
+            cap = cv2.VideoCapture(0)
             while True:
                 images = []
                 for i in range(2):
-                    image = cv2.imread("test.jpg")
-                    image = cv2.resize(image, (224, 224))
-                    images.append(image)
+                    ret, frame = cap.read()
+                    if not ret:
+                        break
+
+                    # preserving aspect ratio
+                    frame = cv2.resize(frame, (frame.shape[1] * 224 // frame.shape[0], 224))
+                    frame = center_crop(frame, (224, 224))
+                    images.append(frame)
                 images = np.array(images)
                 images = images / 255.0
-                images = torch.tensor(images).permute(0, 3, 1, 2).float()
+                images = torch.tensor(images).permute(0, 3, 1, 2).float().unsqueeze(0)
                 images = images.to(device)
                 output = model(images)
-                print("output: ", output)
+                # print("output: ", output)
+                viz_mg_data((images[0], None), model, live=True)
     else:
-        pipeline = DataPipeline(frames_per_item=2)
-        viz_loader = DataLoader(train_dataset, batch_size=args.bs, shuffle=False)
+        pipeline = DataPipeline(frames_per_item=args.frames_per_item)
+        val_dataset = pipeline.get_dataset('test')
+        viz_loader = DataLoader(val_dataset, batch_size=args.bs, shuffle=False)
         model.load_state_dict(torch.load(args.checkpoint))
         model.eval()
 
--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import cv2
 from typing import Tuple
 import numpy as np
 
-def viz_mg_data(data: Tuple[torch.Tensor, int], model: TurnClassifier):
+def viz_mg_data(data: Tuple[torch.Tensor, int], model: TurnClassifier, live: bool = False):
     images, label = data
     output = model(images.unsqueeze(0))
 
@@ -12,41 +12,21 @@ def viz_mg_data(data: Tuple[torch.Tensor, int], model: TurnClassifier):
         image = images[i].permute(1, 2, 0).detach().cpu().numpy()
         image = cv2.resize(image, (448, 448))
     
-        print("label: ", label)
         print("raw model output: ", torch.nn.functional.softmax(output, dim=0))
         # printing percentage of turning
-        label_str = "label = turning" if torch.argmax(output[0]) == 1 else "label = not turning"
+        if label is not None:
+            print("label: ", label)           
+            label_str = "label = turning" if torch.argmax(output[0]) == 1 else "label = not turning"
+            cv2.putText(image, label_str, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        
         percent_str = f"turn pred: {torch.nn.functional.softmax(output[0])[1].item() * 100:.2f}%"
-        print(label_str)
-        print(percent_str)
-        cv2.putText(image, label_str, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         cv2.putText(image, percent_str, (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        
         cv2.imshow("image", image)
-        cv2.waitKey(0)
-
-def viz_live_data(model: TurnClassifier):
-    cap = cv2.VideoCapture(0)
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        frame = cv2.resize(frame, (448, 448))
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame = torch.tensor(frame).permute(2, 0, 1).float() / 255.0
-        frame = frame.unsqueeze(0)
-        output = model(frame)
-
-        label_str = "label = turning" if torch.argmax(output[0]) == 1 else "label = not turning"
-        percent_str = f"turn pred: {torch.nn.functional.softmax(output[0])[1].item() * 100:.2f}%"
-        cv2.putText(frame, label_str, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        cv2.putText(frame, percent_str, (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        cv2.imshow("frame", frame.permute(1, 2, 0).detach().cpu().numpy())
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
+        if not live:
+            cv2.waitKey(0)
+        else:
+            cv2.waitKey(1)
 
 def center_crop(img: np.ndarray, size: Tuple[int, int]) -> np.ndarray:
     h, w = img.shape[:2]
